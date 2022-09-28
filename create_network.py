8ecd46acb120db0deac727fecdbaa6c6d1045a97
Nicholas Wolczynski <nicholaswolczynski@Nicholass-MacBook-Pro.local>
2022-09-28 11:06:32 -0500

    made changes to communities generation, still not confirmed to work

--- create_network.py
+++ create_network.py
@@ -10,20 +10,23 @@ import os.path
 def create_network(path):
     print('reading in network...')
     G = nx.read_edgelist(path)
-    #identify communities
+    #identify top 5 levels of communities
     print('checking if communities file exists...')
     if os.path.isfile('communities.pkl'):
         print('found communities file, loading communities')
         with open("communities.pkl", "rb") as f:
-            communities_generator = pickle.load(f)
+            communities = pickle.load(f)
     else:
         print('communities file not found, using girvan_newman to generate communities...')
         communities_generator = community.girvan_newman(G)
+        communities = []
+        for i in range(5):
+            communities.append(list(next(communities_generator)))
         with open("communities.pkl", "wb") as f:
-            pickle.dump(communities_generator, f)
+            pickle.dump(np.array(communities), f)
     
     #assign communities to node attributes
-    
+
     
     return G
 
--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from create_network import *
 
 def main():
-    print("Basic Network Exploration")
+    
     G = create_network('higgs-social_network.edgelist')
-    print('hello')
+    
 
 
 if __name__ == "__main__":
