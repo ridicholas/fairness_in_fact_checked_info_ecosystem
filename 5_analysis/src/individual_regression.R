11a7a6ba90d1eb2aad12b0f172def528a10f39d4
Terrence Neumann <terrenceneumann2016@u.northwestern.edu>
2023-01-20 11:05:18 -0600

    individual level regression

---
+++ 5_analysis/src/individual_regression.R
@@ -0,0 +1,42 @@
+library(data.table)
+library(fixest)
+
+results <- fread('~/Desktop/NetworkFairness/fairness_in_fact_checked_info_ecosystem/5_analysis/output/individual_level_regression_data.csv',
+                 colClasses = c('factor', 'factor', 'factor', 'factor', 'factor', 'numeric', 'numeric','numeric', 'numeric', 'numeric','numeric'))
+results <- results[Community != 1,]
+
+results[,Community := factor(Community,
+                             levels = c('3', '56', '43', '72', '34', '127', '12', '28'))]
+
+results[,Intervention:= factor(Intervention, 
+                              levels = c('no_intervention_change_in_belief', 
+                              'TopPredictedByTopic_knowledgable_community_stratified_nodes_visited',
+                              'TopPredictedByTopic_knowledgable_community_nodes_visited',
+                              'TopPredictedByTopic_random_nodes_visited',
+                              'TopPredictedByTopic_random_stratified_nodes_visited',
+                              'TopPredictedByTopic_stratified_nodes_visited',
+                              'TopPredictedByTopic_stratified_stratified_nodes_visited',
+                              'TopPredicted_knowledgable_community_nodes_visited',
+                              'TopPredicted_knowledgable_community_stratified_nodes_visited',
+                              'TopPredicted_random_nodes_visited',
+                              'TopPredicted_random_stratified_nodes_visited',
+                              'TopPredicted_stratified_nodes_visited',
+                              'TopPredicted_stratified_stratified_nodes_visited'))]
+
+
+
+colnames(results) <- c('Rep', 'Node', 'Topic', 'Intervention', 'Community', 'Number.of.Bots.Followed', 'Betweenness.Centrality', 'Impactedness', 'Average.External.Belief', 'Midpoint.Belief', 'Change.in.Belief')
+
+
+
+mod <- feols(Change.in.Belief ~ Rep + Rep:Community + 
+               Intervention + Intervention:Community +
+               Midpoint.Belief + Midpoint.Belief^2 + 
+               Average.External.Belief + Average.External.Belief^2 + 
+               Betweenness.Centrality + Betweenness.Centrality^2 + 
+               Number.of.Bots.Followed + Number.of.Bots.Followed^2 + 
+               Impactedness + Impactedness^2,
+               data = results)
+summary(mod)
+
+coeftable(mod, cluster = ~Community + Rep)
